69e4eb7701f2ca3ea0a86fbdeb517cce93906179
Wenzhi Cui <cwz920716@gmail.com>
2018-02-18 14:20:49 -0600

    P48 done

--- test.lisp
+++ test.lisp
@@ -399,3 +399,25 @@
                                :SPLIT (:USE MEM-INT-HELPER)
                                :PROMOTE :S))
 
+(DEFTHM SUB-CONS-OK
+        (IMPLIES (SUB X Y) (SUB X (CONS A Y)))
+        :INSTRUCTIONS (:PROMOTE :INDUCT :S (:DIVE 1)
+                                :X :TOP :PROMOTE :EXPAND :S (:DEMOTE 2)
+                                (:DIVE 1 1)
+                                :X :TOP
+                                :S :PROMOTE :EXPAND :S-PROP (:DEMOTE 4)
+                                (:DIVE 1)
+                                :EXPAND :S :TOP :PROMOTE (:DEMOTE 5)
+                                (:DEMOTE 3)
+                                :S))
+
+(DEFTHM SUB-A-A-OK (SUB A A)
+        :INSTRUCTIONS (:INDUCT :S (:DEMOTE 2)
+                               (:DIVE 1)
+                               :X :TOP :S :X (:DEMOTE 3)
+                               (:USE (:INSTANCE SUB-CONS-OK (X (CDR A))
+                                                (Y (CDR A))))
+                               :PROMOTE :PROMOTE (:DEMOTE 1)
+                               (:DIVE 1 2)
+                               :S :TOP (:DEMOTE 3)
+                               :S))
