86b292f1990f58fda99eabc18da2791be103471f
Wenzhi Cui <cwz920716@gmail.com>
2018-03-21 16:10:58 -0500

    realize seems good

--- .emacs.el
+++ .emacs.el
@@ -17,3 +17,6 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+(ac-config-default)
+
--- project.lisp
+++ project.lisp
@@ -206,6 +206,8 @@
   (declare (xargs :guard (halidep e)))
   (cdr e))
 
+;; Above looks good
+
 (defun realize-at-1d (e id ctx)
   (declare (xargs :guard (and (natp id)
                               (halide-1dp e)
@@ -213,7 +215,7 @@
   (let* ((dim0 (car (halide-dims e)))
          (rhs (halide-expr e))
          (idp (cons dim0 id)))
-    (expr-eval rhs (cons idp ctx))))
+    (ifix (expr-eval rhs (cons idp ctx)))))
 
 (assert-event (equal 3
                      (realize-at-1d (cons '(f x)
@@ -233,9 +235,47 @@
                                           nil)
                                     )))
 
+(defthm realize-type-correct
+  (implies (and (halidep e)
+                (natp i)
+                (contextp ctx))
+           (integerp (realize-at-1d e i ctx))))
+
 (defun realize-1d-inner (e size id ctx)
   (declare (xargs :guard (and (halide-1dp e)
                               (natp size)
                               (natp id)
                               (contextp ctx))))
-  (cons (realize
+  (if (zp size)
+      nil
+    (cons (realize-at-1d e id ctx)
+          (realize-1d-inner e (1- size) (1+ id) ctx))))
+
+(defun realize-1d (e size ctx)
+  (declare (xargs :guard (and (halide-1dp e)
+                              (natp size)
+                              (contextp ctx))))
+  (realize-1d-inner e size 0 ctx))
+
+(defun induct-risz (n)
+  (if (zp n)
+      0
+    (1+ (induct-risz (1- n)))))
+
+(defthm realize-inner-size-good
+  (implies (and (halide-1dp e)
+                (natp N)
+                (natp i)
+                (contextp ctx))
+           (equal (len (realize-1d-inner e N i ctx))
+                  N))
+  :hints (("Goal"
+           :induct (induct-risz N)))
+  )
+
+(defthm realize-size-good
+  (implies (and (halide-1dp e)
+                (natp N)
+                (contextp ctx))
+           (equal (len (realize-1d e N ctx))
+                  N)))
