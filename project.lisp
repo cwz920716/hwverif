0c6a12fb9b7ee682ab82daef59824b341233dcdb
Wenzhi Cui <cwz920716@gmail.com>
2018-04-16 12:12:04 -0500

    add constant fold

--- project.lisp
+++ project.lisp
@@ -745,3 +745,43 @@
            (equal (exec-stmt (compile-halide e n)
                              ctx)
                   (simulate-1d e 0 n ctx))))
+
+;; Now add constant folding
+
+(defun constant-fold (e)
+  (declare (xargs
+            :guard (exprp e))
+            ;; :measure (...)
+           )
+  (if (atom e)
+      e
+    (let* ((fn (car e))
+           (args (cdr e))
+           (arg1 (car args))
+           (arg1* (cdr args))
+           (arg2 (cadr args))
+           (arg2* (cddr args))
+           (v1 (constant-fold arg1))
+           (v2 (constant-fold arg2)))
+      (case fn
+        (- (if (integerp v1) (- v1) (cons fn (cons v1 arg1*))))
+        (+ (if (and (integerp v1)
+                    (integerp v2))
+               (+ v1 v2)
+             (cons fn (cons v1 (cons v2 arg2*)))))
+        (* (if (and (integerp v1)
+                    (integerp v2))
+               (* v1 v2)
+             (cons fn (cons v1 (cons v2 arg2*)))))
+        ([] (cons fn (cons v1 (cons v2 arg2*))))
+        (alloca (cons fn (cons v1 arg1*)))
+        (otherwise e)))))
+
+(defthm constant-fold-expr (implies (exprp e)
+                                    (exprp (constant-fold e))))
+
+(defthm constant-fold-is-correct
+  (implies (and (exprp e)
+                (contextp a))
+           (equal (expr-eval (constant-fold e) a)
+                  (expr-eval e a))))
